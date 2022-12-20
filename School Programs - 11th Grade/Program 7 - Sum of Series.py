6b15b8261c4d87dc4b34f652e929bfce601d2fc1
Aniketh Chavare <anikethchavare@gmail.com>
2022-12-20 16:23:36 +0530

    Update Program 7 - Sum of Series.py

--- School Programs - 11th Grade/Program 7 - Sum of Series.py
+++ School Programs - 11th Grade/Program 7 - Sum of Series.py
@@ -1,5 +1,7 @@
 # Program 7 - Sum of Series
 
+# Program to calculate the sum of the series 1 + x2 + x3 + x4...
+
 # Variables
 n = int(input("Enter number of terms: "))
 x = int(input("Enter value of x: "))

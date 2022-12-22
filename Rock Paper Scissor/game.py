2c2702e15cadd9ca423ed297894a97842204b7a3
Aniketh Chavare <anikethchavare@gmail.com>
2022-12-22 10:02:58 +0530

    Add files via upload

---
+++ Rock Paper Scissor/game.py
@@ -0,0 +1,45 @@
+# Rock Paper Scissor - Game
+
+# Imports
+import random
+import time
+
+# Variables
+result = ""
+computerChoice = ""
+
+# Play
+def play(choice):
+   computer = random.randrange(1,4)
+
+   if computer == 1: # Computer's Choice - Paper
+      computerChoice = "Paper"
+
+      if (choice == "paper"):
+         result = "Draw"
+      elif (choice == "rock"):
+         result = "You Lose"
+      elif (choice== "scissor"):
+         result = "You Win"
+   
+   elif computer==2: # Computer's Choice - Rock
+      computerChoice = "Rock"
+   
+      if (choice=="rock"):
+         result = "Draw"
+      elif (choice=="paper"):
+         result = "You Win"
+      elif (choice == "scissor"):
+         result = "You Lose"
+   
+   elif computer==3: # Computer's Choice - Scissor
+      computerChoice = "Scissor"
+   
+      if (choice=="rock"):
+         result = "You Win"
+      elif (choice=="scissor"):
+         result = "Draw"
+      elif (choice == "paper"):
+         result = "You Lose"
+
+   return {"result": result, "computer": computerChoice}
